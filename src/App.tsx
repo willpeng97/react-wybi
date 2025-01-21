596096304aa88a6e28ad080071c269a738d6f4a8
Will <willpeng97@gmail.com>
2025-01-22 00:14:12 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css';
-import './styles/dashboard.css';
-import './styles/sidebar.css';
 import { router } from './Router';
 
 const App: React.FC = () => {
